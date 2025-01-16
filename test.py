286e758c7c4816f02eb78e3ed151599d050dd303
Dr zola <73894117+ZolaBaksa@users.noreply.github.com>
2025-01-16 16:56:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 
 micom_info = sys.argv[1]
 
-# Micom 빈 영역을 0xFF로 채운 0x80000 바이트의 배열 생성
+# Micom 빈 영역을 0xFF로 채운 배열 생성
 if micom_info == 'TMPM3H':
     bin_data = bytearray([0xFF] * 0x80000)
     hex_line = ":08FEF000"
@@ -14,9 +14,9 @@ elif micom_info == 'TMPM380':
 elif micom_info == 'TMPM383':
     bin_data = bytearray([0xFF] * 0x20000)
     hex_line = ":08FEF000"
-elif micom_info == 'RF1308':
-    bin_data = bytearray([0xFF] * 0x80000)
-    hex_line = ":08000000"
+elif micom_info == 'TMPM4KH':
+    bin_data = bytearray([0xFF] * 0x40000)  # 256KB**
+    hex_line = ":080FE800"  # 적절한 hex_line을 설정**
 
 target_dir = sys.argv[2]
 target_hex = sys.argv[3]
@@ -38,10 +38,7 @@ def hex_to_bin(hex_file, bin_file):
                     data = bytes.fromhex(line[9:9 + length * 2])
                     bin_data[(b_address+address):(b_address+address) + length] = data
                 elif record_type == 4:  # 주소 레코드
-                    if micom_info == 'RF1308':
-                        data = int(line[9:9 + length * 2], 16) - int('FFF8', 16)
-                    else:
-                        data = int(line[9:9 + length * 2], 16)
+                    data = int(line[9:9 + length * 2], 16)
                     b_address = data << 16
 
     # .bin 파일로 저장
@@ -59,14 +56,13 @@ bin_file = f"{target_dir}\\{os.path.splitext(target_hex)[0]}.bin"  # 출력 BIN 
 bin_data = hex_to_bin(hex_file, bin_file)
 byte_sum = calculate_byte_sum(bin_data)
 
-if micom_info == 'TMPM380':
+if micom_info == 'TMPM380' or micom_info == 'TMPM4KH':
     byte_sum = 0x0000FFFF & byte_sum
-    print(f'바이트 합: 0x{byte_sum:04X}')  # 16진수로 출력
+    print(f'바이트 합: 0x{byte_sum:04X}')  # 16진수로 출력**
 else:
     print(f'바이트 합: 0x{byte_sum:08X}')  # 16진수로 출력
 
 def update_define_value(file_path, csum_define, new_value):
-    # 1. 특정 파일을 'utf-8' 형식으로 연다.
     print("update_define_value road")
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -76,9 +72,9 @@ def update_define_value(file_path, csum_define, new_value):
         print("Error: The file is empty.")
         return ""
 
-    # 2. '#define SW_CSUM' 으로 시작하는 위치를 찾는다.
     define_pattern = re.compile(r'^#define\s+' + csum_define)
     define_pattern_saa = re.compile(r'^#define\s+SW_PNO')
+    define_pattern_pno = re.compile(r'^#define\s+SW_PART_NUMBER_SEND\s+"SAA(\d+)"')
     print("here is the point")
     saa = ""
     matched = False
@@ -87,9 +83,13 @@ def update_define_value(file_path, csum_define, new_value):
         if micom_info == 'TMPM3H' and define_pattern_saa.match(line):
             saa = line[-9:-1]
             print(f"Found SW_PNO: {saa}")
+        if micom_info == 'TMPM4KH' and define_pattern_pno.match(line):
+            match = define_pattern_pno.search(line)
+            if match:
+                saa = match.group(1)
+                print(f"Found SW_PART_NUMBER_SEND: {saa}")
         if define_pattern.match(line):
             print(f"Found {csum_define} at line {i}: {line.strip()}")
-            # 3. 해당 라인을 '#define {csum_define} ' 에 변수의 값을 '0x' 로 시작하고 대문자로 표현된 16진수 값으로 기록한다.
             lines[i] = f'#define {csum_define}\t{new_value}\n'
             matched = True
             break
@@ -98,7 +98,6 @@ def update_define_value(file_path, csum_define, new_value):
         print(f"Error: No matching line found for #define {csum_define} in the file.")
         return saa
 
-    # 변경된 내용을 파일에 다시 쓴다.
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(lines)
     return saa
@@ -114,56 +113,44 @@ csum_define = sys.argv[6]
 
 csum_file = f'{pjt_dir}\\{csum_src}'
 
-if micom_info == 'TMPM380':
+if micom_info == 'TMPM380' or micom_info == 'TMPM4KH':
     new_csum = '0x' + f'{byte_sum:04X}'
 else:
     new_csum = '0x' + f'{byte_sum:08X}'
 new_saa = update_define_value(csum_file, csum_define, new_csum)
+print(f"new_saa: {new_saa}")
 
 def modify_hex_line(line):
-    # 행의 구조 파악
     if not line.startswith(hex_line):
         return line
 
-    if micom_info == 'TMPM3H' or micom_info == 'RF1308':
-        # 데이터 추출
+    if micom_info == 'TMPM3H':
         data = line[9:41]  # :10FEF800 이후의 데이터 (16바이트)
 
-        # CSUM 계산
         csum = byte_sum
 
-        # FFFFFFFF에서 CSUM을 뺀 값 계산
         ffffffff_minus_csum = 0xFFFFFFFF - byte_sum
 
-        # 결과 생성
         csum_hex = f'{csum:08X}'
         csum_hex = f'{csum_hex[-2:]}{csum_hex[-4:-2]}{csum_hex[-6:-4]}{csum_hex[-8:-6]}'
         ffffffff_minus_csum_hex = f'{ffffffff_minus_csum:08X}'
         ffffffff_minus_csum_hex = f'{ffffffff_minus_csum_hex[-2:]}{ffffffff_minus_csum_hex[-4:-2]}{ffffffff_minus_csum_hex[-6:-4]}{ffffffff_minus_csum_hex[-8:-6]}'
-        # 새로운 데이터 생성
         new_data = f'{csum_hex}{ffffffff_minus_csum_hex}'
 
-        # 새로운 행 생성
         new_line = f'{hex_line}{new_data}{line[25:]}'
-    elif micom_info == 'TMPM380':
-        # 데이터 추출
+    elif micom_info == 'TMPM380' or micom_info == 'TMPM4KH':
         data = line[9:41]  # :10FEF800 이후의 데이터 (16바이트)
 
-        # CSUM 계산
         csum = byte_sum
 
-        # FFFFFFFF에서 CSUM을 뺀 값 계산
         ffff_minus_csum = 0xFFFF - byte_sum
 
-        # 결과 생성
         csum_hex = f'{csum:04X}'
         csum_hex = f'{csum_hex[-2:]}{csum_hex[-4:-2]}'
         ffff_minus_csum_hex = f'{ffff_minus_csum:04X}'
         ffff_minus_csum_hex = f'{ffff_minus_csum_hex[-2:]}{ffff_minus_csum_hex[-4:-2]}'
-        # 새로운 데이터 생성
         new_data = f'{ffff_minus_csum_hex}{csum_hex}'
 
-        # 새로운 행 생성
         new_line = f'{hex_line}{new_data}{line[17:]}'
     print(new_line)
     return new_line
@@ -177,3 +164,4 @@ def process_hex_file(input_file, output_file):
 input_file = f"{target_dir}\\{target_hex}"
 output_file = f'{target_dir}\\{os.path.splitext(target_hex)[0]}_{new_csum}_SAA{new_saa}.hex'
 process_hex_file(input_file, output_file)
+
